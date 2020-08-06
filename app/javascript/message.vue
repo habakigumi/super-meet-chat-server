673a9f82935e9e1a012f54a2695e916b207a6e86
taichanne30 <taichanne30@senshu-u.jp>
2020-08-07 04:53:06 +0900

    Fix condition

--- app/javascript/message.vue
+++ app/javascript/message.vue
@@ -5,7 +5,7 @@
     <div v-for="msg in messages" class="message" :class="`message-${msg.super_chat_type}`" :key="`msg-${msg.id}`">
       <p class="message__sender">{{ msg.sender }}</p>
       <p v-if="msg.super_chat_type !== 'normal'" class="message__value">{{ msg.value.toLocaleString('ja-JP', { style: 'currency', currency: 'JPY' }) }}</p>
-      <p v-if="msg.super_chat_type !== 'normal' && msg.text !== ''" class="message__text">{{ msg.text }}</p>
+      <p v-if="msg.super_chat_type === 'normal' || msg.text !== ''" class="message__text">{{ msg.text }}</p>
     </div>
 
   </template>
